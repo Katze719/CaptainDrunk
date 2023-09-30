04460d4caabe0ae1bfd3ddbe69b81b1c5ef5ab2c
Katze719 <pauldorn1234@gmail.com>
2023-09-30 14:26:58 +0200

    feat: :sparkles: added footer

--- helpers.py
+++ helpers.py
@@ -4,4 +4,5 @@ EMBED_COLOR=0xFEC200
 
 def simple_embed(title, text=''):
     embed = discord.Embed(title=title,description=text, color=EMBED_COLOR)
+    embed.set_footer(text="Drink responsibly!")
     return embed
